2d5d87bad470c64f01841fe540a305a36ff69e9b
Dilip Reddy Gaddam <gaddamdg@amazon.com>
2026-03-02 12:52:06 -0800

    SAPHANA changes foe query federation

--- athena-jdbc/src/main/java/com/amazonaws/athena/connectors/jdbc/manager/JdbcSplitQueryBuilder.java
+++ athena-jdbc/src/main/java/com/amazonaws/athena/connectors/jdbc/manager/JdbcSplitQueryBuilder.java
@@ -19,6 +19,7 @@
  */
 package com.amazonaws.athena.connectors.jdbc.manager;
 
+import com.amazonaws.athena.connector.lambda.connection.EnvironmentConstants;
 import com.amazonaws.athena.connector.lambda.domain.Split;
 import com.amazonaws.athena.connector.lambda.domain.predicate.Constraints;
 import com.amazonaws.athena.connector.lambda.domain.predicate.OrderByField;
@@ -185,7 +186,9 @@ public abstract class JdbcSplitQueryBuilder
             throws SQLException
     {
         if (constraints.getQueryPlan() != null) {
-            SqlDialect sqlDialect = getSqlDialect();
+            boolean casingFilter = isCatalogCasingFilterApplied(split);
+            LOGGER.info("Is Upper case catalog casing filter applied {}", casingFilter);
+            SqlDialect sqlDialect = getSqlDialect(casingFilter);
             return prepareStatementWithCalciteSql(jdbcConnection, constraints, sqlDialect, split);
         }
         List<TypeAndValue> accumulator = new ArrayList<>();
@@ -410,6 +413,11 @@ public abstract class JdbcSplitQueryBuilder
         return AnsiSqlDialect.DEFAULT;
     }
 
+    protected SqlDialect getSqlDialect(boolean catalogCasingFilterApplied)
+    {
+        return getSqlDialect();
+    }
+
     protected PreparedStatement prepareStatementWithCalciteSql(
             final Connection jdbcConnection,
             final Constraints constraints,
@@ -456,6 +464,12 @@ public abstract class JdbcSplitQueryBuilder
             LOGGER.error("Failed to prepare statement with Calcite", e);
             throw new RuntimeException("Failed to prepare statement with Calcite", e);
         }
+    }
+
+    protected boolean isCatalogCasingFilterApplied(Split split)
+    {
+        return EnvironmentConstants.UPPERCASE_ONLY.equals(split.getProperties()
+                .getOrDefault(EnvironmentConstants.CATALOG_CASING_FILTER, null));
     }
 
     private PreparedStatement handleDataTypesForPreparedStatement(PreparedStatement statement,
--- athena-saphana/src/main/java/com/amazonaws/athena/connectors/saphana/SaphanaQueryStringBuilder.java
+++ athena-saphana/src/main/java/com/amazonaws/athena/connectors/saphana/SaphanaQueryStringBuilder.java
@@ -36,6 +36,7 @@ import org.apache.arrow.vector.types.Types;
 import org.apache.arrow.vector.types.pojo.ArrowType;
 import org.apache.arrow.vector.types.pojo.Field;
 import org.apache.arrow.vector.types.pojo.Schema;
+import org.apache.calcite.sql.SqlDialect;
 import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -134,6 +135,13 @@ public class SaphanaQueryStringBuilder extends JdbcSplitQueryBuilder
             final Split split)
             throws SQLException
     {
+        if (constraints.getQueryPlan() != null) {
+            boolean casingFilter = isCatalogCasingFilterApplied(split);
+            LOGGER.info("Is Upper case catalog casing filter applied {}", casingFilter);
+            SqlDialect sqlDialect = getSqlDialect(casingFilter);
+            return prepareStatementWithCalciteSql(jdbcConnection, constraints, sqlDialect, split);
+        }
+
         StringBuilder sql = new StringBuilder();
         String columnNames = tableSchema.getFields().stream()
                 .map(this::quoteField)
@@ -228,6 +236,19 @@ public class SaphanaQueryStringBuilder extends JdbcSplitQueryBuilder
         LOGGER.debug("Field name to quote {}", name);
         name = name.replace(SAPHANA_QUOTE_CHARACTER, SAPHANA_QUOTE_CHARACTER + SAPHANA_QUOTE_CHARACTER);
         return SAPHANA_QUOTE_CHARACTER + name + SAPHANA_QUOTE_CHARACTER;
+    }
+
+    @Override
+    protected SqlDialect getSqlDialect()
+    {
+        return SAPHanaSqlDialect.DEFAULT;
+    }
+
+    @Override
+    protected SqlDialect getSqlDialect(boolean catalogCasingFilterApplied)
+    {
+        return new SAPHanaSqlDialect(SAPHanaSqlDialect.DEFAULT_CONTEXT,
+                catalogCasingFilterApplied);
     }
 
     private String quoteField(Field field)
